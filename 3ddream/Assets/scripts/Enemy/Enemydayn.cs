59a3c921a8dd3fc6dea8cdeee4843fc729ed4be1
l1niktol <xexevheburator@gmail.com>
2024-03-01 21:22:24 +0300

    yes

--- 3ddream/Assets/scripts/Enemy/Enemydayn.cs
+++ 3ddream/Assets/scripts/Enemy/Enemydayn.cs
@@ -7,20 +7,17 @@ public class Enemydayn : MonoBehaviour
     // Start is called before the first frame update
 
     public float speed = 1.0f;
-    public float health = 100f;
-    public float damage = 20f;
+    public float health = 100;
+    public float damage = 20;
+
+
    
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        
     }
-    private void OnCollisionEnter(Collision collision)
-    {
-        if(gameObject.tag == "Bullet")
-        {
-            health -= damage;
-        }
-    }
+    
 }
