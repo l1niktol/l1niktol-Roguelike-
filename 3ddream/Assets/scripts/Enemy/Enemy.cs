21417dee8d250d518cbd2a1935f0478fd681832f
l1niktol <xexevheburator@gmail.com>
2024-03-11 19:49:10 +0300

    with health system

--- 3ddream/Assets/scripts/Enemy/Enemy.cs
+++ 3ddream/Assets/scripts/Enemy/Enemy.cs
@@ -2,21 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemydayn : MonoBehaviour
+public class Enemy : MonoBehaviour
 {
     // Start is called before the first frame update
 
     public float speed = 1.0f;
-    
+    public float health = 100f;
 
 
-   
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
-        
+
     }
-    
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
 }
--- 3ddream/Assets/scripts/weapon/Bullet.cs
+++ 3ddream/Assets/scripts/weapon/Bullet.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float bulletLife = 3;
+    public float damage = 1f;
 
     private void Awake()
     {
@@ -14,6 +15,12 @@ public class Bullet : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
             Destroy(gameObject, bulletLife);
-        
+
+        Enemy enemy = collision.gameObject.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage);
+        }
+
     }
 }
